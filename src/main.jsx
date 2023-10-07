c9396d20b3fb43a4469d2899de062e2661b0db8c
Developer-Shahadat <shahadatbci95@gmail.com>
2023-10-07 23:25:24 +0600

    react router,tailwind,daisy ui install and set up

---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+import Root from './root/Root';
+import Home from './pages/Home/Home';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children:[{
+      path: "/home",
+      element:<Home></Home>,
+    }]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
